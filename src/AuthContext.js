c11b8c48b83029e5ed39274499c644666010a105
marville001 <mwangimartin1904@gmail.com>
2021-05-14 13:33:25 +0300

    Update

--- src/AuthContext.js
+++ src/AuthContext.js
@@ -6,8 +6,6 @@ const AuthContext = createContext();
 const AuthProvider = ({ children, history }) => {
   const [user, setuser] = useState({name:"Martin"});
   // const [pending, setPending] = useState(false);
-  
-
   const value = {
     user
   }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import {BrowserRouter as Router} from 'react-router-dom'
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { BrowserRouter as Router } from "react-router-dom";
 
-import './index.css';
+import "./index.css";
+import { AuthProvider } from "./AuthContext";
 
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
-  document.getElementById('root')
+  <AuthProvider>
+    <Router>
+      <App />
+    </Router>
+  </AuthProvider>,
+  document.getElementById("root")
 );
