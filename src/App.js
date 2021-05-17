afe0fe82315c763e6f7608e1c8e9d08655ca1ef6
marville001 <mwangimartin1904@gmail.com>
2021-05-17 13:04:20 +0300

    Authentication complete

--- src/App.js
+++ src/App.js
@@ -1,31 +1,20 @@
 import React, { useContext, useEffect, useState } from "react";
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, Redirect } from "react-router-dom";
 import ProtectedRoute from "./ProtectedRoute";
 
-import {AuthContext} from './AuthContext'
+import { AuthContext } from "./AuthContext";
 
 const Login = React.lazy(() => import("./pages/Login"));
 const Index = React.lazy(() => import("./pages/Home/Index"));
 
-
 const App = () => {
-  const [isAuth, setIsAuth] = useState(false);
-
-  const {user} = useContext(AuthContext);
-
-  useEffect(() => {
-    if(user.uid){
-      setIsAuth(true);
-    }else{
-      setIsAuth(false)
-    }
-  }, [])
+  const { user } = useContext(AuthContext);
 
   return (
     <React.Suspense fallback={() => <div>Loading...</div>}>
       <Switch>
         <Route exact path="/login" component={Login} />
-        <Route path="/" isAuth={isAuth} component={Index} />
+        <Route path="/" component={Index} />
       </Switch>
     </React.Suspense>
   );
