ef319c1d3d1f4c97c437cd24507f56d6f416f45c
marville001 <mwangimartin1904@gmail.com>
2021-09-14 12:13:46 +0300

    Updated bar graph

--- src/components/Bar.jsx
+++ src/components/Bar.jsx
@@ -5,7 +5,7 @@ const data = {
   labels: ["Red", "Blue", "Yellow"],
   datasets: [
     {
-      label: "# of Votes",
+      label: "# of vehicles",
       data: [1, 2, 3],
       backgroundColor: [
         "rgba(255, 99, 132, 0.2)",
